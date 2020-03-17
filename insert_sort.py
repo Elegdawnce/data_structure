1b03a60f54c976171143b880d8168f29ae47c170
Elegdawnce <570814569@qq.com>
2020-03-17 22:22:59 +0800

    Update insert_sort.py

--- insert_sort.py
+++ insert_sort.py
@@ -1,32 +1,6 @@
 import numpy as np
 import random
 
-def helper(nums, left, right):
-    if left >= right:
-        return
-    # choose left as pivot
-    pivot = left
-    # randomly select one pivot
-    # pivot = nums.index(random.sample(nums[left:right+1],1)[0])
-    
-    # swap pivot with end
-    nums[pivot], nums[right] = nums[right], nums[pivot]
-    # num smaller than pivot
-    small = left - 1
-    # left pivot right
-    for i in range(left, right):
-        if nums[i] < nums[right]:
-            small += 1
-            if small != i:
-                nums[i], nums[small] = nums[small], nums[i]
-    
-    # the position of pivot
-    small += 1  
-    # exchange again
-    nums[small], nums[right] = nums[right], nums[small]
-    # iteration
-    helper(nums, left, small-1)
-    helper(nums, small+1, right)
 
 
 def insert_sort(nums):
